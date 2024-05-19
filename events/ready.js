d3a1df304478be791786b9a7386af3c23bb825dd
coding777e <168589076+coding777e@users.noreply.github.com>
2024-05-19 12:54:42 +0200

    Vraisready.js

--- events/ready.js
+++ events/ready.js
@@ -45,7 +45,7 @@ console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}
 
 const serverCount = client.guilds.cache.size;
 setInterval(() => client.user.setActivity({ 
-  name:`lavaLink GlaceYT`, 
+  name:`・❓▏/Help`, 
   type: ActivityType.Listening }), 10000);
 client.errorLog = config.errorLog
   
@@ -71,4 +71,3 @@ client.errorLog = config.errorLog
 
 
 */
-
